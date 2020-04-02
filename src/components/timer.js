27601487717b797cdb695c5feea9e18bd5d1d7d0
Shpakova Alena <shpakovadev@gmail.com>
2020-04-02 14:43:11 +0300

    style

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,22 +1,61 @@
-import React from 'react'
-import './timer.css'
+import React, { useState } from 'react'
+import './timer.scss'
 
 const Timer = () => {
+  const [seconds, setSeconds] = useState(0)
+  const [startTime, setStartTime] = useState(undefined)
+  const [intervalID, setIntervalID] = useState(undefined)
+
+  const startTimer = () => {
+    setStartTime(Date.now())
+    setIntervalID(
+      setInterval(() => {
+        console.log(
+          'startTime, Date.now() - startTime',
+          startTime,
+          Date.now() - startTime
+        )
+        // setSeconds(Date.now() - startTime)
+      }, 1000)
+    )
+  }
+
+  const stopTimer = () => {
+    clearInterval(intervalID)
+    setSeconds(0)
+  }
+
+  const pauseTimer = () => {}
+
   return (
     <div className="timer">
       <h1 className="timer__title">Tаймер</h1>
       <div className="timer__main">
         <div className="timer__count">
-          <h1>00:00</h1>
+          <h1>00:{seconds}</h1>
         </div>
+        <div className="timer__startTime">{startTime}</div>
         <div className="timer__input">
           <input type="text" placeholder="Минуты" />
-          <input type="text" placeholder="Секунды" />
+          <input
+            type="text"
+            placeholder="Секунды"
+            value={seconds}
+            onChange={e => {
+              setSeconds(e.target.value)
+            }}
+          />
         </div>
-        <div className="timer__actions">
-          <button className="timer__action-cancel">Отмена</button>
-          <button className="timer__action-start">Старт</button>
-          <button className="timer__action-pause">Пауза</button>
+        <div className="timer__action">
+          <button className="timer__action-cancel" onClick={stopTimer}>
+            Отмена
+          </button>
+          <button className="timer__action-start" onClick={startTimer}>
+            Старт
+          </button>
+          <button className="timer__action-pause" onClick={pauseTimer}>
+            Пауза
+          </button>
         </div>
         <div className="progressbar">[progressbar]</div>
         <div className="timer__statistics">
